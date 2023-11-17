3ca29f264203d3960a5f730ab0c9050aa5cd6caa
Alfredo De Santiago <145381140+Yotarokujo27@users.noreply.github.com>
2023-11-16 22:00:19 -0800

    Added damgable and health objects
    Added damagable and health objects and am going to start the animation part

--- RubyAdvetureP3DeSantiago/Assets/Scripts/RubyController.cs
+++ RubyAdvetureP3DeSantiago/Assets/Scripts/RubyController.cs
@@ -8,7 +8,13 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+
+    public int health { get { return currentHealth; }}
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -26,6 +32,13 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+                if (invincibleTimer < 0)
+                isInvincible = false; 
+        }
     }
 
    void FixedUpdate()
@@ -38,8 +51,18 @@ public class RubyController : MonoBehaviour
 
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
+   
     {
+      if(amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
