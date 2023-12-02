c4a13e48a13d4b0305e0814a6555b0be98403115
Alfredo De Santiago <145381140+Yotarokujo27@users.noreply.github.com>
2023-12-01 18:59:16 -0800

    Finished the project
    Finally the finished the project

--- RubyAdvetureP3DeSantiago/Assets/Scripts/RubyController.cs
+++ RubyAdvetureP3DeSantiago/Assets/Scripts/RubyController.cs
@@ -30,6 +30,7 @@ public class RubyController : MonoBehaviour
     Vector2 lookDirection = new Vector2(1,0);
 
     AudioSource audioSource;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -105,8 +106,8 @@ public class RubyController : MonoBehaviour
 
             isInvincible = true;
             invincibleTimer = timeInvincible;
-
             PlaySound(hitSound);
+
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
@@ -122,8 +123,8 @@ public class RubyController : MonoBehaviour
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
-
         PlaySound(throwSound);
+
     }
 
     public void PlaySound(AudioClip clip)
